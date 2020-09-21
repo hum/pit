2dad9eede0696da133b2574c1639d3e48b77344d
hum <matonohamartin@gmail.com>
2020-09-21 12:31:35 +0200

    added hash and cat function for pit files

--- pit/data.py
+++ pit/data.py
@@ -1,8 +1,21 @@
 ## manages the .pit directiory
-
 import os
+import hashlib
 
 PIT_DIR = '.pit'
 
 def init():
 	os.makedirs(PIT_DIR)
+	os.makedirs(f'{PIT_DIR}/objects')
+
+def hash_object(data):
+	old = hashlib.sha1(data).hexdigest()
+	
+	with open(f'{PIT_DIR}/objects/{oid}', 'wb') as out:
+		out.write(data)
+	
+	return oid
+
+def get_file(oid):
+	with open(f'{PIT_DIR}/objects/{oid}', 'rb') as f:
+		return f.read()
