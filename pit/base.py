79356626c4efe810efa6e66ef4cc9f855c1ac8b4
hum <matonohamartin@gmail.com>
2020-09-23 10:01:47 +0200

    added pit log command

--- pit/base.py
+++ pit/base.py
@@ -1,5 +1,9 @@
 from . import data
+import itertools
+import operator
 import os
+
+from collections import namedtuple
 
 def write_tree(directory = '.'):
 	entries = []
@@ -89,3 +93,23 @@ def commit(message):
 	data.set_HEAD(oid)
 
 	return oid
+
+Commit = namedtuple('Commit', ['tree', 'parent', 'message'])
+
+def get_commit(oid):
+	parent = None
+
+	commit = data.get_object(oid, 'commit').decode()
+	lines = iter(commit.splitlines())
+	for line in itertools.takewhile(operator.truth, lines):
+		key, value = line.split(' ', 1)
+
+		if key == 'tree':
+			tree = value
+		elif key == 'parent':
+			parent = value
+		else:
+			assert False, f'Unknown field {key}'
+
+	message = '\n'.join(lines)
+	return Commit(tree=tree, parent=parent, message=message)
